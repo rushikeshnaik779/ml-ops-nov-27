2d5a9474708f560c50b7c3ea074f8a4fde07d69b
Rushikesh <rushikeshnaik779@gmail.com>
2022-12-01 20:49:12 -0800

    added few new files to test and main py file at right position, also added init files so it can use as packages

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+# from data_extraction import data_extraction
+# from data_preparation import data_train_val_test_split, imputing_missing_value, under_sample
+# from evaluation import predict, metrics_cal
+# from model_training import train_model
