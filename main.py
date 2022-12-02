258818dceb225bb22d0bd4d6cfd5c9ec6700ebea
Rushikesh <rushikeshnaik779@gmail.com>
2022-12-01 21:00:48 -0800

    adding mlflow

---
+++ main.py
@@ -0,0 +1,21 @@
+from src.data_extraction import data_extraction
+from src.data_preparation import data_train_val_test_split, imputing_missing_value, under_sample
+from src.evaluation import metrics_cal, predict
+from src.model_training import train_model
+
+if __name__ == "__main__": 
+    path = 'data/weatherAUS.csv'
+    data = data_extraction(path)
+    print(data.shape)
+    train_df, val_df, test_df = data_train_val_test_split(data)
+    
+    train_df, val_df, test_df = imputing_missing_value(train_df, val_df, test_df)
+    
+    x, y = under_sample(train_df)
+    model = train_model(x, y)
+    test_pred = predict(model, test_df.drop(['RainTomorrow'], axis=1))
+    val_pred = predict(model, val_df.drop(['RainTomorrow'], axis=1))
+    
+    
+    print(metrics_cal(test_df['RainToday'],test_pred))
+    print(metrics_cal(val_df['RainToday'],val_pred))
